3ac01605b1dc58c83d147fe082a84767d9e6aedb
KKhushhalR2405 <reddykhushhal2405@gmail.com>
2021-11-29 22:32:36 +0530

    added signup url

--- todo/views.py
+++ todo/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
-
+from django.contrib.auth.forms import UserCreationForm
 # Create your views here.
+
+def signupuser(request):
+    return render(request,'todo/signupuser.html',{'form':UserCreationForm()})
