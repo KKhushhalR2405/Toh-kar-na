07698077b1e2dd7bc2207bc7b2f2d348e6d4bc24
KKhushhalR2405 <reddykhushhal2405@gmail.com>
2021-11-30 00:52:15 +0530

    added backend functionalities

--- todo/views.py
+++ todo/views.py
@@ -1,6 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from django.db import IntegrityError
+from django.contrib.auth import login
 # Create your views here.
 
 def signupuser(request):
-    return render(request,'todo/signupuser.html',{'form':UserCreationForm()})
+    if request.method=='GET':
+        return render(request,'todo/signupuser.html',{'form':UserCreationForm()})
+
+    else:
+        #Create a new user
+        if request.POST["password1"] == request.POST["password2"]:
+            try:
+                user = User.objects.create_user(request.POST['username'],password = request.POST['password1'])
+                user.save()
+                login(request,user)
+                return redirect('currenttodos')
+            except IntegrityError:
+                return render(request,'todo/signupuser.html',{'form':UserCreationForm(), 'error':"User name already exist"})
+                
+
+        else:
+            return render(request,'todo/signupuser.html',{'form':UserCreationForm(), 'error':"Password did not match"})
+
+
+def currenttodos(request):
+    return render(request,'todo/currenttodos.html')
