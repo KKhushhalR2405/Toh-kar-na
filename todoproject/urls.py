4b71fc44b6b39e7b8e8fa2d7052ac32f7df3990a
KKhushhalR2405 <reddykhushhal2405@gmail.com>
2021-11-29 22:32:54 +0530

    added urls

--- todoproject/urls.py
+++ todoproject/urls.py
@@ -15,7 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from todo import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+
+    # Auth
+    path('signup/',views.signupuser, name = 'signupuser')
+
+    # Todos
 ]
