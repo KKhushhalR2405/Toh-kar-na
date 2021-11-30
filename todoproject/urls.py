f7be64b7b5433c499b348ceebabebc167edfbecf
KKhushhalR2405 <reddykhushhal2405@gmail.com>
2021-11-30 23:09:01 +0530

    url updated

--- todoproject/urls.py
+++ todoproject/urls.py
@@ -22,7 +22,9 @@ urlpatterns = [
 
     # Auth
     path('signup/',views.signupuser, name = 'signupuser'),
+    path('logout/',views.logoutuser, name = 'logoutuser'),
 
     # Todos
     path('current/',views.currenttodos, name = 'currenttodos'),
+    path('',views.home, name = 'home'),
 ]
