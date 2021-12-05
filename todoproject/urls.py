7861f4b4c3ed0c621748c5047296f8ad5807dc8d
KKhushhalR2405 <reddykhushhal2405@gmail.com>
2021-12-05 21:29:11 +0530

    added login url

--- todoproject/urls.py
+++ todoproject/urls.py
@@ -22,6 +22,7 @@ urlpatterns = [
 
     # Auth
     path('signup/',views.signupuser, name = 'signupuser'),
+    path('login/',views.loginuser, name = 'loginuser'),
     path('logout/',views.logoutuser, name = 'logoutuser'),
 
     # Todos
