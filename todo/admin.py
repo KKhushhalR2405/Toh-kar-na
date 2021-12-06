f637f346cc773c20d6c8bb6f360e33f61df95923
KKhushhalR2405 <reddykhushhal2405@gmail.com>
2021-12-06 21:58:38 +0530

    registered todo model

--- todo/admin.py
+++ todo/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from .models import Todo
 # Register your models here.
+
+class TodoAdmin(admin.ModelAdmin):
+    readonly_fields = ('created',)
+
+admin.site.register(Todo,TodoAdmin)
