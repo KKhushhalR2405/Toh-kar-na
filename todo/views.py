3995b335a1d122d5178b0a58c6dd74bb475c00bc
KKhushhalR2405 <reddykhushhal2405@gmail.com>
2021-12-06 23:00:13 +0530

    .

--- todo/views.py
+++ todo/views.py
@@ -1,52 +1,72 @@
 from django.http.response import HttpResponse
-from django.shortcuts import render,redirect
-from django.contrib.auth.forms import UserCreationForm,AuthenticationForm
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.models import User
 from django.db import IntegrityError
-from django.contrib.auth import login,logout,authenticate
+from django.contrib.auth import login, logout, authenticate
+from .forms import TodoForm
 # Create your views here.
 
+
 def signupuser(request):
-    if request.method=='GET':
-        return render(request,'todo/signupuser.html',{'form':UserCreationForm()})
+    if request.method == 'GET':
+        return render(request, 'todo/signupuser.html', {'form': UserCreationForm()})
 
     else:
-        #Create a new user
+        # Create a new user
         if request.POST["password1"] == request.POST["password2"]:
             try:
-                user = User.objects.create_user(request.POST['username'],password = request.POST['password1'])
+                user = User.objects.create_user(
+                    request.POST['username'], password=request.POST['password1'])
                 user.save()
-                login(request,user)
+                login(request, user)
                 return redirect('currenttodos')
             except IntegrityError:
-                return render(request,'todo/signupuser.html',{'form':UserCreationForm(), 'error':"User name already exist"})
-                
+                return render(request, 'todo/signupuser.html', {'form': UserCreationForm(), 'error': "User name already exist"})
 
         else:
-            return render(request,'todo/signupuser.html',{'form':UserCreationForm(), 'error':"Password did not match"})
+            return render(request, 'todo/signupuser.html', {'form': UserCreationForm(), 'error': "Password did not match"})
 
 
 def currenttodos(request):
-    return render(request,'todo/currenttodos.html')
+    return render(request, 'todo/currenttodos.html')
+
 
 def logoutuser(request):
-    if request.method=='POST':
+    if request.method == 'POST':
         logout(request)
         return redirect('home')
 
+
 def home(request):
-    return render(request,'todo/home.html')
+    return render(request, 'todo/home.html')
+
 
 def loginuser(request):
-    if request.method=='GET':
-        return render(request,'todo/loginuser.html',{'form':AuthenticationForm()})
+    if request.method == 'GET':
+        return render(request, 'todo/loginuser.html', {'form': AuthenticationForm()})
 
     else:
-        #Login user
-        user = authenticate(request,username = request.POST["username"], password=request.POST["password"])
+        # Login user
+        user = authenticate(
+            request, username=request.POST["username"], password=request.POST["password"])
         if user:
-            login(request,user)
+            login(request, user)
             return redirect('currenttodos')
         else:
-            return render(request,'todo/loginuser.html',{'form':AuthenticationForm(),"error":"User do not exist"}) 
-        
+            return render(request, 'todo/loginuser.html', {'form': AuthenticationForm(), "error": "User do not exist"})
+
+
+def createtodo(request):
+    if request.method == 'GET':
+        return render(request, 'todo/createtodo.html', {'form': TodoForm()})
+    else:
+        try:
+
+            form = TodoForm(request.POST)
+            newtodo = form.save(commit=False)
+            newtodo.user = request.user
+            newtodo.save()
+            return redirect('currenttodos')
+        except ValueError:
+            return render(request, 'todo/createtodo.html', {'form': TodoForm(),'error':'Some Error occured'})
