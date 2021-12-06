0a43e0f9b0721ca3fb8430f91517e0a5d249ccda
KKhushhalR2405 <reddykhushhal2405@gmail.com>
2021-12-06 21:59:00 +0530

    updated model

--- todo/models.py
+++ todo/models.py
@@ -7,6 +7,9 @@ class Todo(models.Model):
     title = models.CharField(max_length=100)
     memo = models.TextField(blank=True)
     created = models.DateTimeField(auto_now=False, auto_now_add=True)
-    datecompleted = models.DateTimeField(null=True)
+    datecompleted = models.DateTimeField(null=True,blank=True)
     important = models.BooleanField(default=True)
     user = models.ForeignKey(User,on_delete=models.CASCADE)
+
+    def __str__(self) -> str:
+        return self.title
