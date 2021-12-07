bbe3ad35e2b0fa61c6dd108d3bcfe0bcab919c29
KKhushhalR2405 <reddykhushhal2405@gmail.com>
2021-12-07 22:11:11 +0530

    updated urls

--- todoproject/urls.py
+++ todoproject/urls.py
@@ -30,4 +30,5 @@ urlpatterns = [
     path('create/',views.createtodo, name = 'createtodo'),
     path('',views.home, name = 'home'),
     path('todo/<int:todo_pk>',views.viewtodo, name = 'viewtodo'),
+    path('todo/<int:todo_pk>/complete>',views.completetodo, name = 'completetodo'),
 ]
